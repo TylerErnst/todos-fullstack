8a26bd4db4a91c9c0ac483583acdb8c05a4cd4ea
TylerErnst <tcernst5@gmail.com>
2024-05-23 11:19:19 -0600

    connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express'
+import 'dotenv/config'
+
+import mongoConfig from './config.js';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 app.get('/', (req,res) => {
     res.json('Hello (From server)');
 })
 
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT)
+    console.log('Listening on port: ', PORT);
+    mongoConfig()
 })
