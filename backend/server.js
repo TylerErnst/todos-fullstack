249ef17e4248858fef0504b13211e80bc7041426
TylerErnst <tcernst5@gmail.com>
2024-05-23 14:36:33 -0600

    added cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import 'dotenv/config'
 
 import mongoConfig from './config.js';
@@ -8,6 +9,7 @@ import todoRoutes from './routes/todoRoutes.js'
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors())
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
 
@@ -18,5 +20,5 @@ app.get('/', (req,res) => {
 
 app.listen(PORT, () => {
     console.log('Listening on port: ', PORT);
-    mongoConfig()
+    mongoConfig();
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    async function getTodos() {
+      try {
+        const response = await fetch('http://localhost:8080/api/todos');
+        const data = await response.json();
+        console.log(data);
+        setTodos(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getTodos();
+  }, [])
+
   const textRef = useRef();
   const completeRef = useRef();
 
@@ -27,6 +43,12 @@ function App() {
         <br /><br />
         <button>Add Todo</button>
       </form>
+
+      <br /><br />
+
+      {todos.map((todo) => 
+        <p style={{ textDecoration: todo.complete ? 'line-through' : '' }}key={todo.text}>{todo.text}</p>
+      )}
     </>
   )
 }
