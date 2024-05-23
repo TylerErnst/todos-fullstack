12a9ca94a9c4f172c07a175193ad762cc5b24f0f
TylerErnst <tcernst5@gmail.com>
2024-05-23 11:41:01 -0600

    added a index route

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from 'express'
 import 'dotenv/config'
 
 import mongoConfig from './config.js';
+import Todo from './models/TodoModel.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.get('/', (req,res) => {
     res.json('Hello (From server)');
+})
+
+app.get('/api/todos', async (req,res) => {
+    try {
+        const todos = await Todo.find();
+        res.status(200).json(todos);
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json(error);
+    }
 })
 
 app.listen(PORT, () => {
